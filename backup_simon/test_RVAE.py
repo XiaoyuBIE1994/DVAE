fc266b694023fea1f74e7c280f686406ba412a92
XiaoyuBIE Mac <biexiaoyu1994@gmail.com>
2020-02-26 23:38:30 +0800

    adapt simon's test code to mac

--- backup_simon/test_RVAE.py
+++ backup_simon/test_RVAE.py
@@ -2,6 +2,7 @@
 test your model after training
 """
 
+import os
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,11 +19,8 @@ input_dim = 513
 latent_dim = 16   
 hidden_dim = 128
 activation = torch.tanh
-rec_over_z = True
 num_LSTM = 1
 num_dense_enc = 1
-bidir_enc_s = False
-bidir_dec = False
 
 #%% STFT parameters
 
@@ -36,7 +34,34 @@ verbose=False
 #%% test parameters
 
 use_gpu = False
-saved_model = '/Users/xiaoyu/WorkStation/saved_model_Xiaoyu/WSJ0_2020-02-22-12h32_UniEnc_UniDec_NoRecZ_z_dim=16/RVAE_final_epoch207.pt'
+# rnn rec z
+# saved_dir = '/Users/xiaoyu/WorkStation/saved_model_Simon/WSJ0_2019-07-15-10h14_RVAE_RNNenc_RNNdec_RecZ_latent_dim=16'
+# saved_model = os.path.join(saved_dir, 'final_model_RVAE_epoch121.pt')
+# rec_over_z = True
+# bidir_enc_s = False
+# bidir_dec = False
+
+# # rnn no rec z
+# saved_dir = '/Users/xiaoyu/WorkStation/saved_model_Simon/WSJ0_2019-08-01-12h45_RVAE_RNNenc_RNNdec_NoRecZ_latent_dim=16'
+# saved_model = os.path.join(saved_dir, 'final_model_RVAE_epoch72.pt')
+# rec_over_z = False
+# bidir_enc_s = False
+# bidir_dec = False
+
+# # brnn rec z
+# saved_dir = '/Users/xiaoyu/WorkStation/saved_model_Simon/WSJ0_2019-07-15-10h01_RVAE_BRNNenc_BRNNdec_RecZ_latent_dim=16'
+# saved_model = os.path.join(saved_dir, 'final_model_RVAE_epoch145.pt')
+# rec_over_z = True
+# bidir_enc_s = True
+# bidir_dec = True
+
+# # brnn no rec z
+saved_dir = '/Users/xiaoyu/WorkStation/saved_model_Simon/WSJ0_2019-08-02-08h52_RVAE_BRNNenc_BRNNdec_NoRecZ_latent_dim=16'
+saved_model = os.path.join(saved_dir, 'final_model_RVAE_epoch120.pt')
+rec_over_z = False
+bidir_enc_s = True
+bidir_dec = True
+
 device = 'cpu'
 
 #%%
@@ -55,14 +80,19 @@ vae.eval()
 if use_gpu:
     vae = vae.cuda()
 
-#%%
 
 #plt.close('all')    
 
-data_dir = '/local_scratch/sileglai/datasets/clean_speech/TIMIT/TEST'
-file_list = librosa.util.find_files(data_dir, ext='wav')
+# data_dir = '/local_scratch/sileglai/datasets/clean_speech/TIMIT/TEST'
+# file_list = librosa.util.find_files(data_dir, ext='wav')
 #wavfile = random.choice(file_list)
-wavfile = '/Users/xiaoyu/WorkStation/Data/clean_speech/wsj0_si_et_05/440/440c0210.wav'
+data_dir = '/Users/xiaoyu/WorkStation/Data/clean_speech/wsj0_si_et_05/440'
+audiofile_list = []
+file_list = ['440c020a.wav', '440c020b.wav', '440c020c.wav', '440c020d.wav', '440c020e.wav']
+for file in file_list:
+    audio_file = os.path.join(data_dir, file)
+    audiofile_list.append(audio_file)
+
 
 wlen = int(wlen_sec*fs) # window length of 64 ms
 wlen = np.int(np.power(2, np.ceil(np.log2(wlen)))) # next power of 2
@@ -70,62 +100,70 @@ nfft = wlen
 hop = np.int(hop_percent*wlen) # hop size
 win = np.sin(np.arange(.5,wlen-.5+1)/wlen*np.pi); # sine analysis window
 
-x, fs_x = sf.read(wavfile)    
-x = x/np.max(np.abs(x))
-X = librosa.stft(x, n_fft=nfft, hop_length=hop, 
-                             win_length=wlen,
-                             window=win) # STFT
-data_orig = np.abs(X)**2
+for num, wavfile in enumerate(audiofile_list):
+    x, fs_x = sf.read(wavfile)    
+    x = x/np.max(np.abs(x))
+    X = librosa.stft(x, n_fft=nfft, hop_length=hop, 
+                                win_length=wlen,
+                                window=win) # STFT
+    data_orig = np.abs(X)**2
 
-with torch.no_grad():
+    with torch.no_grad():
+        
+    #    data_orig = torch.from_numpy(data_orig.astype(np.float32))
+    #    data_orig = data_orig.unsqueeze(0) # add a dimension in axis 0
+    #    data_orig = data_orig.permute(-1,0,1) # (sequence_len, batch_size, input_dim)
+    #    data_orig = data_orig.to(device)
+        
+        data_orig = data_orig.T
+        data_orig = torch.from_numpy(data_orig.astype(np.float32))
+        data_orig = data_orig.to(device)
+        
+        data_recon, mean, logvar, z = vae(data_orig)
+        mean = mean.detach().numpy().T
+        data_recon = data_recon.detach().numpy().T
+        data_orig = data_orig.detach().numpy().T
+        
+    #    mean = mean[:,0,:].detach().numpy().T
+    #    data_recon = data_recon[:,0,:].detach().numpy().T
+    #    data_orig = data_orig[:,0,:].detach().numpy().T
+
+    plt.figure()
+    plt.subplot(3, 1, 1)
+    librosa.display.specshow(librosa.power_to_db(data_orig), y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
+    #librosa.display.specshow(librosa.power_to_db(x_train[0:500,:].T), y_axis='log', sr=fs, hop_length=hop)
+    plt.set_cmap('jet')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Original spectrogram')
+
+    plt.subplot(3, 1, 2)
+    plt.imshow(mean, origin='lower')
+    plt.set_cmap('jet')
+    plt.colorbar(format='%+2.0f dB')
+
+    plt.subplot(3, 1, 3)
+    librosa.display.specshow(librosa.power_to_db(data_recon), x_axis='time', y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
+    #librosa.display.specshow(librosa.power_to_db(data_decoded.T), x_axis='time', y_axis='log', sr=fs, hop_length=hop)
+    plt.set_cmap('jet')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Reconstructed spectrogram')
+
+    X_recon = np.sqrt(data_recon)*np.exp(1j*np.angle(X))
+
+    x_recon = librosa.istft(X_recon, hop_length=hop, win_length=wlen, window=win)
+    x_orig = x
+
+    scale = 1/(np.maximum(np.max(np.abs(x_recon)),np.max(np.abs(x_orig))))*0.9
     
-#    data_orig = torch.from_numpy(data_orig.astype(np.float32))
-#    data_orig = data_orig.unsqueeze(0) # add a dimension in axis 0
-#    data_orig = data_orig.permute(-1,0,1) # (sequence_len, batch_size, input_dim)
-#    data_orig = data_orig.to(device)
-    
-    data_orig = data_orig.T
-    data_orig = torch.from_numpy(data_orig.astype(np.float32))
-    data_orig = data_orig.to(device)
-    
-    data_recon, mean, logvar, z = vae(data_orig)
-    mean = mean.detach().numpy().T
-    data_recon = data_recon.detach().numpy().T
-    data_orig = data_orig.detach().numpy().T
-    
-#    mean = mean[:,0,:].detach().numpy().T
-#    data_recon = data_recon[:,0,:].detach().numpy().T
-#    data_orig = data_orig[:,0,:].detach().numpy().T
+    orig_file = os.path.join(saved_dir, 'signal_origin-{}.wav'.format(num))
+    recon_file = os.path.join(saved_dir, 'signal_recon-{}.wav'.format(num))
+    plot_file = os.path.join(saved_dir, 'resynthesis-{}.png'.format(num))
 
-plt.figure()
-plt.subplot(3, 1, 1)
-librosa.display.specshow(librosa.power_to_db(data_orig), y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
-#librosa.display.specshow(librosa.power_to_db(x_train[0:500,:].T), y_axis='log', sr=fs, hop_length=hop)
-plt.set_cmap('jet')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Original spectrogram')
+    librosa.output.write_wav(orig_file, scale*x_orig, fs)
+    librosa.output.write_wav(recon_file, scale*x_recon, fs)
+    plt.savefig(plot_file)
 
-plt.subplot(3, 1, 2)
-plt.imshow(mean, origin='lower')
-plt.set_cmap('jet')
-plt.colorbar(format='%+2.0f dB')
-
-plt.subplot(3, 1, 3)
-librosa.display.specshow(librosa.power_to_db(data_recon), x_axis='time', y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
-#librosa.display.specshow(librosa.power_to_db(data_decoded.T), x_axis='time', y_axis='log', sr=fs, hop_length=hop)
-plt.set_cmap('jet')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Reconstructed spectrogram')
-
-X_recon = np.sqrt(data_recon)*np.exp(1j*np.angle(X))
-
-x_recon = librosa.istft(X_recon, hop_length=hop, win_length=wlen, window=win)
-x_orig = x
-
-scale = 1/(np.maximum(np.max(np.abs(x_recon)),np.max(np.abs(x_orig))))*0.9
-
-librosa.output.write_wav('./signal_recon.wav', scale*x_recon, fs)
-librosa.output.write_wav('./signal_orig.wav', scale*x_orig, fs)
+    print('=====> {} file {} reconstruction finished'.format(num, wavfile))
 
 
 
--- backup_simon/test_VAE.py
+++ backup_simon/test_VAE.py
@@ -13,6 +13,8 @@ import soundfile as sf
 import librosa
 from VAEs import VAE
 import random
+import librosa.display
+import os
 
 
 plt.close('all')
@@ -35,7 +37,12 @@ verbose=False
 #%% test parameters
 
 use_gpu = False
-saved_model = '/local_scratch/sileglai/recherche/python/speech_enhancement/SE_FFNN_RNN_VAEs_pytorch/saved_model/VAE_first_test_11_june/final_model_VAE_epoch125.pt'
+
+# saved_model = '/local_scratch/sileglai/recherche/python/speech_enhancement/SE_FFNN_RNN_VAEs_pytorch/saved_model/VAE_first_test_11_june/final_model_VAE_epoch125.pt'
+
+saved_dir = '/Users/xiaoyu/WorkStation/saved_model_Simon/WSJ0_2019-07-15-10h21_FFNN_VAE_latent_dim=16'
+saved_model = os.path.join(saved_dir, 'final_model_RVAE_epoch65.pt')
+
 device = 'cpu'
 
 #%%
@@ -56,10 +63,18 @@ if use_gpu:
 
 #plt.close('all')    
 
-data_dir = './data'
-file_list = librosa.util.find_files(data_dir, ext='wav')
-wavfile = random.choice(file_list)
+# data_dir = './data'
+# file_list = librosa.util.find_files(data_dir, ext='wav')
+# wavfile = random.choice(file_list)
 #wavfile = '/local_scratch/sileglai/datasets/test_Simon.wav'
+
+data_dir = '/Users/xiaoyu/WorkStation/Data/clean_speech/wsj0_si_et_05/440'
+audiofile_list = []
+file_list = ['440c020a.wav', '440c020b.wav', '440c020c.wav', '440c020d.wav', '440c020e.wav']
+for file in file_list:
+    audio_file = os.path.join(data_dir, file)
+    audiofile_list.append(audio_file)
+
 
 wlen = int(wlen_sec*fs) # window length of 64 ms
 wlen = np.int(np.power(2, np.ceil(np.log2(wlen)))) # next power of 2
@@ -67,59 +82,63 @@ nfft = wlen
 hop = np.int(hop_percent*wlen) # hop size
 win = np.sin(np.arange(.5,wlen-.5+1)/wlen*np.pi); # sine analysis window
 
-x, fs_x = sf.read(wavfile)    
-x = x/np.max(np.abs(x))
-X = librosa.stft(x, n_fft=nfft, hop_length=hop, 
-                             win_length=wlen,
-                             window=win) # STFT
-data_orig = np.abs(X)**2
+for num, wavfile in enumerate(audiofile_list):
+    x, fs_x = sf.read(wavfile)    
+    x = x/np.max(np.abs(x))
+    X = librosa.stft(x, n_fft=nfft, hop_length=hop, 
+                                win_length=wlen,
+                                window=win) # STFT
+    data_orig = np.abs(X)**2
 
-with torch.no_grad():
-    
-    data_orig = data_orig.T
-    data_orig = torch.from_numpy(data_orig.astype(np.float32))
-    data_orig = data_orig.to(device)
-    
-    data_recon, mean, logvar, z = vae(data_orig)
-    mean = mean.detach().numpy().T
-    data_recon = data_recon.detach().numpy().T
-    
-    data_orig = data_orig.detach().numpy().T
+    with torch.no_grad():
+        
+        data_orig = data_orig.T
+        data_orig = torch.from_numpy(data_orig.astype(np.float32))
+        data_orig = data_orig.to(device)
+        
+        data_recon, mean, logvar, z = vae(data_orig)
+        mean = mean.detach().numpy().T
+        data_recon = data_recon.detach().numpy().T
+        
+        data_orig = data_orig.detach().numpy().T
+        
 
-#%%
-       
-import librosa.display
-plt.figure()
-plt.subplot(3, 1, 1)
-librosa.display.specshow(librosa.power_to_db(data_orig), y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
-#librosa.display.specshow(librosa.power_to_db(x_train[0:500,:].T), y_axis='log', sr=fs, hop_length=hop)
-plt.set_cmap('jet')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Original spectrogram')
+    plt.figure()
+    plt.subplot(3, 1, 1)
+    librosa.display.specshow(librosa.power_to_db(data_orig), y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
+    #librosa.display.specshow(librosa.power_to_db(x_train[0:500,:].T), y_axis='log', sr=fs, hop_length=hop)
+    plt.set_cmap('jet')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Original spectrogram')
 
-plt.subplot(3, 1, 2)
-plt.imshow(mean, origin='lower')
-plt.set_cmap('jet')
-plt.colorbar(format='%+2.0f dB')
+    plt.subplot(3, 1, 2)
+    plt.imshow(mean, origin='lower')
+    plt.set_cmap('jet')
+    plt.colorbar(format='%+2.0f dB')
 
-plt.subplot(3, 1, 3)
-librosa.display.specshow(librosa.power_to_db(data_recon), x_axis='time', y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
-#librosa.display.specshow(librosa.power_to_db(data_decoded.T), x_axis='time', y_axis='log', sr=fs, hop_length=hop)
-plt.set_cmap('jet')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Reconstructed spectrogram')
+    plt.subplot(3, 1, 3)
+    librosa.display.specshow(librosa.power_to_db(data_recon), x_axis='time', y_axis='log', sr=fs, hop_length=hop)#, vmin=-50, vmax=20)
+    #librosa.display.specshow(librosa.power_to_db(data_decoded.T), x_axis='time', y_axis='log', sr=fs, hop_length=hop)
+    plt.set_cmap('jet')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Reconstructed spectrogram')
 
-#%%
+    X_recon = np.sqrt(data_recon)*np.exp(1j*np.angle(X))
 
-X_recon = np.sqrt(data_recon)*np.exp(1j*np.angle(X))
+    x_recon = librosa.istft(X_recon, hop_length=hop, win_length=wlen, window=win)
+    x_orig = x
 
-x_recon = librosa.istft(X_recon, hop_length=hop, win_length=wlen, window=win)
-x_orig = x
+    scale = 1/(np.maximum(np.max(np.abs(x_recon)),np.max(np.abs(x_orig))))*0.9
 
-scale = 1/(np.maximum(np.max(np.abs(x_recon)),np.max(np.abs(x_orig))))*0.9
+    orig_file = os.path.join(saved_dir, 'signal_origin-{}.wav'.format(num))
+    recon_file = os.path.join(saved_dir, 'signal_recon-{}.wav'.format(num))
+    plot_file = os.path.join(saved_dir, 'resynthesis-{}.png'.format(num))
 
-librosa.output.write_wav('./signal_recon.wav', scale*x_recon, fs)
-librosa.output.write_wav('./signal_orig.wav', scale*x_orig, fs)
+    librosa.output.write_wav(orig_file, scale*x_orig, fs)
+    librosa.output.write_wav(recon_file, scale*x_recon, fs)
+    plt.savefig(plot_file)
+
+    print('=====> {} file {} reconstruction finished'.format(num, wavfile))
 
 
 
