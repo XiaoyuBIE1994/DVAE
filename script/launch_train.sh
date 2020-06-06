4ca416270efbffaf47d5021db8e13ce061c0682c
XiaoyuBIE Mac <biexiaoyu1994@gmail.com>
2020-06-06 21:57:37 +0800

    experiments, all models

--- script/launch_train.sh
+++ script/launch_train.sh
@@ -1,9 +1,3 @@
-oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_dmm.sh \
-        -l/host=1/gpudevice=1,walltime=90:00:00 \
-        -p "cluster='perception' OR cluster='kinovis'" \
-        -t besteffort \
-        -t idempotent
-
 oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_storn.sh \
         -l/host=1/gpudevice=1,walltime=90:00:00 \
         -p "cluster='perception' OR cluster='kinovis'" \
