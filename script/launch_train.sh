1bd05cf22603d57513cd94557b69cf00a0a00c2b
XiaoyuBIE Mac <biexiaoyu1994@gmail.com>
2020-06-06 21:56:37 +0800

    experiments, all models

--- script/launch_train.sh
+++ script/launch_train.sh
@@ -4,37 +4,37 @@ oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_dmm.sh \
         -t besteffort \
         -t idempotent
 
-oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_seq_storn.sh \
+oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_storn.sh \
         -l/host=1/gpudevice=1,walltime=90:00:00 \
         -p "cluster='perception' OR cluster='kinovis'" \
         -t besteffort \
         -t idempotent
 
-oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_seq_vrnn.sh \
+oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_vrnn.sh \
         -l/host=1/gpudevice=1,walltime=90:00:00 \
         -p "cluster='perception' OR cluster='kinovis'" \
         -t besteffort \
         -t idempotent
 
-oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_seq_srnn.sh \
+oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_srnn.sh \
         -l/host=1/gpudevice=1,walltime=90:00:00 \
         -p "cluster='perception' OR cluster='kinovis'" \
         -t besteffort \
         -t idempotent
 
- oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_seq_rvae_Causal.sh \
+ oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_rvae_Causal.sh \
         -l/host=1/gpudevice=1,walltime=90:00:00 \
         -p "cluster='perception' OR cluster='kinovis'" \
         -t besteffort \
         -t idempotent
 
- oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_seq_NonCausal.sh \
+ oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_NonCausal.sh \
         -l/host=1/gpudevice=1,walltime=90:00:00 \
         -p "cluster='perception' OR cluster='kinovis'" \
         -t besteffort \
         -t idempotent
 
- oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_seq_dsae.sh \
+ oarsub -S /scratch/virgo/xbie/Code/rvae-speech/script/train_dsae.sh \
         -l/host=1/gpudevice=1,walltime=90:00:00 \
         -p "cluster='perception' OR cluster='kinovis'" \
         -t besteffort \
