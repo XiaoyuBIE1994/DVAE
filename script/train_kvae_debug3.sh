9ebd949f958c20670d133048459fe3ad5865d2bc
XiaoyuBIE Mac <biexiaoyu1994@gmail.com>
2020-07-21 23:43:22 +0200

    KVAE experiments on learning rate

--- script/train_kvae_debug3.sh
+++ script/train_kvae_debug3.sh
@@ -3,4 +3,4 @@ singularity exec \
                 --bind /scratch/virgo/xbie/:/mnt/xbie/ \
                 /scratch/virgo/xbie/Simgs/test \
                 python /mnt/xbie/Code/rvae-speech/train_model_kvae.py \
-                /mnt/xbie/Code/rvae-speech/config/kvae_debug/cfg_kvae_scale5e-1.ini
+                /mnt/xbie/Code/rvae-speech/config/kvae_debug/cfg_kvae_lr3e-4.ini
